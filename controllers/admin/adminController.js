0dde00e1dd2f7fb9cb08e61c190bbc971b9eb41e
sai-sreekhar <sai.sreekhar@gmail.com>
2023-03-21 17:35:36 +0530

    feat: added admin routes for teams

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -1,7 +1,9 @@
 const AppError = require("../../utils/appError");
 const catchAsync = require("../../utils/catchAsync");
 const User = require("../../models/userModel");
-const eHackTeams = require("../../models/eHackTeamModel");
+const eHackTeamModel = require("../../models/eHackTeamModel");
+const impetusTeamModel = require("../../models/impetusTeamModel");
+const innoventureTeamModel = require("../../models/innoventureTeamModel");
 
 exports.getAllCounts = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -247,5 +249,155 @@ exports.getTradingWorkshopUsers = catchAsync(async (req, res, next) => {
     No_Of_Users_Registered_For_Tading_Workshop:
       tradingWorkshopRegisteredUsers.length,
     tradingWorkshopRegisteredUsers,
+  });
+});
+
+exports.getEhackDetails = catchAsync(async (req, res, next) => {
+  const eHackTeams = await eHackTeamModel
+    .find(
+      {},
+      {
+        _id: 0,
+        noOfTimesTeamNameChanged: 0,
+        noOfPendingRequests: 0,
+        __v: 0,
+        teamLeaderId: 0,
+      }
+    )
+    .populate("members", {
+      email: 1,
+      firstName: 1,
+      lastName: 1,
+      mobileNumber: 1,
+      _id: 0,
+    });
+
+  const ehackTeamsWith1Member = [];
+  const ehackTeamsWith2Members = [];
+  const ehackTeamsWith3Members = [];
+  const ehackTeamsWith4Members = [];
+
+  for (let i = 0; i < eHackTeams.length; i++) {
+    if (eHackTeams[i].members.length === 1) {
+      ehackTeamsWith1Member.push(eHackTeams[i]);
+    } else if (eHackTeams[i].members.length === 2) {
+      ehackTeamsWith2Members.push(eHackTeams[i]);
+    } else if (eHackTeams[i].members.length === 3) {
+      ehackTeamsWith3Members.push(eHackTeams[i]);
+    } else if (eHackTeams[i].members.length === 4) {
+      ehackTeamsWith4Members.push(eHackTeams[i]);
+    }
+  }
+
+  return res.status(200).json({
+    No_of_EHack_Teams: eHackTeams.length,
+    No_of_EHack_Teams_With_1_Member: ehackTeamsWith1Member.length,
+    No_of_EHack_Teams_With_2_Members: ehackTeamsWith2Members.length,
+    No_of_EHack_Teams_With_3_Members: ehackTeamsWith3Members.length,
+    No_of_EHack_Teams_With_4_Members: ehackTeamsWith4Members.length,
+    E_Hack_Teams_With_1_Member: ehackTeamsWith1Member,
+    E_Hack_Teams_With_2_Members: ehackTeamsWith2Members,
+    E_Hack_Teams_With_3_Members: ehackTeamsWith3Members,
+    E_Hack_Teams_With_4_Members: ehackTeamsWith4Members,
+  });
+});
+
+exports.getImpetusDetails = catchAsync(async (req, res, next) => {
+  const impetusTeams = await impetusTeamModel
+    .find(
+      {},
+      {
+        _id: 0,
+        noOfTimesTeamNameChanged: 0,
+        noOfPendingRequests: 0,
+        __v: 0,
+        teamLeaderId: 0,
+      }
+    )
+    .populate("members", {
+      email: 1,
+      firstName: 1,
+      lastName: 1,
+      mobileNumber: 1,
+      _id: 0,
+    });
+
+  const impetusTeamsWith1Member = [];
+  const impetusTeamsWith2Members = [];
+  const impetusTeamsWith3Members = [];
+  const impetusTeamsWith4Members = [];
+
+  for (let i = 0; i < impetusTeams.length; i++) {
+    if (impetusTeams[i].members.length === 1) {
+      impetusTeamsWith1Member.push(impetusTeams[i]);
+    } else if (impetusTeams[i].members.length === 2) {
+      impetusTeamsWith2Members.push(impetusTeams[i]);
+    } else if (impetusTeams[i].members.length === 3) {
+      impetusTeamsWith3Members.push(impetusTeams[i]);
+    } else if (impetusTeams[i].members.length === 4) {
+      impetusTeamsWith4Members.push(impetusTeams[i]);
+    }
+  }
+
+  return res.status(200).json({
+    No_of_Impetus_Teams: impetusTeams.length,
+    No_of_Impetus_Teams_With_1_Member: impetusTeamsWith1Member.length,
+    No_of_Impetus_Teams_With_2_Members: impetusTeamsWith2Members.length,
+    No_of_Impetus_Teams_With_3_Members: impetusTeamsWith3Members.length,
+    No_of_Impetus_Teams_With_4_Members: impetusTeamsWith4Members.length,
+    Impetus_Teams_With_1_Member: impetusTeamsWith1Member,
+    Impetus_Teams_With_2_Members: impetusTeamsWith2Members,
+    Impetus_Teams_With_3_Members: impetusTeamsWith3Members,
+    Impetus_Teams_With_4_Members: impetusTeamsWith4Members,
+  });
+});
+
+exports.getInnoventureDetails = catchAsync(async (req, res, next) => {
+  const innoventureTeams = await innoventureTeamModel
+    .find(
+      {},
+      {
+        _id: 0,
+        noOfTimesTeamNameChanged: 0,
+        noOfPendingRequests: 0,
+        __v: 0,
+        teamLeaderId: 0,
+      }
+    )
+    .populate("members", {
+      email: 1,
+      firstName: 1,
+      lastName: 1,
+      mobileNumber: 1,
+      _id: 0,
+    });
+
+  const innoventureTeamsWith1Member = [];
+  const innoventureTeamsWith2Members = [];
+  const innoventureTeamsWith3Members = [];
+  const innoventureTeamsWith4Members = [];
+
+  for (let i = 0; i < innoventureTeams.length; i++) {
+    if (innoventureTeams[i].members.length === 1) {
+      innoventureTeamsWith1Member.push(innoventureTeams[i]);
+    } else if (innoventureTeams[i].members.length === 2) {
+      innoventureTeamsWith2Members.push(innoventureTeams[i]);
+    } else if (innoventureTeams[i].members.length === 3) {
+      innoventureTeamsWith3Members.push(innoventureTeams[i]);
+    } else if (innoventureTeams[i].members.length === 4) {
+      innoventureTeamsWith4Members.push(innoventureTeams[i]);
+    }
+  }
+
+  return res.status(200).json({
+    No_of_Innoventure_Teams: innoventureTeams.length,
+    No_of_Innoventure_Teams_With_1_Member: innoventureTeamsWith1Member.length,
+    No_of_Innoventure_Teams_With_2_Members: innoventureTeamsWith2Members.length,
+    No_of_Innoventure_Teams_With_3_Members: innoventureTeamsWith3Members.length,
+    No_of_Innoventure_Teams_With_4_Members: innoventureTeamsWith4Members.length,
+    Innoventure_Teams_With_1_Member: innoventureTeamsWith1Member,
+    Innoventure_Teams_With_2_Members: innoventureTeamsWith2Members,
+    Innoventure_Teams_With_3_Members: innoventureTeamsWith3Members,
+    Innoventure_Teams_With_4_Members: innoventureTeamsWith4Members,
   });
 });
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -9,8 +9,8 @@ adminRouter.route("/user/impetus").get(adminController.getImpetusUsers);
 adminRouter.route("/user/innoventure").get(adminController.getInnoventureUsers);
 adminRouter.route("/user/etalk").get(adminController.getEtalkUsers);
 adminRouter.route("/user/tradingworkshop").get(adminController.getTradingWorkshopUsers);
-// adminRouter.route("/team/ehack").post(adminController.basicAuthLogIn);
-// adminRouter.route("/team/impetus").post(adminController.basicAuthLogIn);
-// adminRouter.route("/team/innoventure").post(adminController.basicAuthLogIn);
+adminRouter.route("/team/ehack").get(adminController.getEhackDetails);
+adminRouter.route("/team/impetus").get(adminController.getImpetusDetails);
+adminRouter.route("/team/innoventure").get(adminController.getInnoventureDetails);
 
 module.exports = adminRouter;
