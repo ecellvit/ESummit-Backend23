75581df8e6619113e50080a0d82c6fac5c6bf806
sai-sreekhar <sai.sreekhar@gmail.com>
2023-03-23 23:06:45 +0530

    style: refactored code

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -4,6 +4,7 @@ const User = require("../../models/userModel");
 const eHackTeamModel = require("../../models/eHackTeamModel");
 const impetusTeamModel = require("../../models/impetusTeamModel");
 const innoventureTeamModel = require("../../models/innoventureTeamModel");
+const { registerTypes } = require("../../utils/constants");
 
 exports.getAllCounts = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -243,8 +244,10 @@ exports.getLoggedInUsers = catchAsync(async (req, res, next) => {
 });
 
 exports.getEhackUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find(
-    {},
+  const eHackRegisteredUsers = await User.find(
+    {
+      "registeredEvents.1": registerTypes.REGISTERED,
+    },
     {
       _id: 0,
       loginType: 0,
@@ -258,16 +261,11 @@ exports.getEhackUsers = catchAsync(async (req, res, next) => {
       eHackTeamRole: 0,
       impetusTeamRole: 0,
       innoventureTeamRole: 0,
+      registeredEvents: 0,
       date: 0,
       __v: 0,
     }
   );
-  const eHackRegisteredUsers = [];
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].registeredEvents && users[i].registeredEvents[1] === 1) {
-      eHackRegisteredUsers.push(users[i]);
-    }
-  }
 
   res.status(200).json({
     message: "Data Fetched Successfully",
@@ -277,8 +275,10 @@ exports.getEhackUsers = catchAsync(async (req, res, next) => {
 });
 
 exports.getImpetusUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find(
-    {},
+  const impetusRegisteredUsers = await User.find(
+    {
+      "registeredEvents.0": registerTypes.REGISTERED,
+    },
     {
       _id: 0,
       loginType: 0,
@@ -292,16 +292,11 @@ exports.getImpetusUsers = catchAsync(async (req, res, next) => {
       eHackTeamRole: 0,
       impetusTeamRole: 0,
       innoventureTeamRole: 0,
+      registeredEvents: 0,
       date: 0,
       __v: 0,
     }
   );
-  const impetusRegisteredUsers = [];
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].registeredEvents && users[i].registeredEvents[0] === 1) {
-      impetusRegisteredUsers.push(users[i]);
-    }
-  }
 
   res.status(200).json({
     message: "Data Fetched Successfully",
@@ -311,8 +306,10 @@ exports.getImpetusUsers = catchAsync(async (req, res, next) => {
 });
 
 exports.getInnoventureUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find(
-    {},
+  const innoventureRegisteredUsers = await User.find(
+    {
+      "registeredEvents.2": registerTypes.REGISTERED,
+    },
     {
       _id: 0,
       loginType: 0,
@@ -326,17 +323,11 @@ exports.getInnoventureUsers = catchAsync(async (req, res, next) => {
       eHackTeamRole: 0,
       impetusTeamRole: 0,
       innoventureTeamRole: 0,
+      registeredEvents: 0,
       date: 0,
       __v: 0,
     }
   );
-
-  const innoventureRegisteredUsers = [];
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].registeredEvents && users[i].registeredEvents[2] === 1) {
-      innoventureRegisteredUsers.push(users[i]);
-    }
-  }
 
   res.status(200).json({
     message: "Data Fetched Successfully",
@@ -346,8 +337,10 @@ exports.getInnoventureUsers = catchAsync(async (req, res, next) => {
 });
 
 exports.getEtalkUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find(
-    {},
+  const eTalkRegisteredUsers = await User.find(
+    {
+      "registeredEvents.3": registerTypes.REGISTERED,
+    },
     {
       _id: 0,
       loginType: 0,
@@ -361,17 +354,11 @@ exports.getEtalkUsers = catchAsync(async (req, res, next) => {
       eHackTeamRole: 0,
       impetusTeamRole: 0,
       innoventureTeamRole: 0,
+      registeredEvents: 0,
       date: 0,
       __v: 0,
     }
   );
-
-  const eTalkRegisteredUsers = [];
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].registeredEvents && users[i].registeredEvents[3] === 1) {
-      eTalkRegisteredUsers.push(users[i]);
-    }
-  }
 
   res.status(200).json({
     message: "Data Fetched Successfully",
@@ -381,8 +368,10 @@ exports.getEtalkUsers = catchAsync(async (req, res, next) => {
 });
 
 exports.getTradingWorkshopUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find(
-    {},
+  const tradingWorkshopRegisteredUsers = await User.find(
+    {
+      "registeredEvents.4": registerTypes.REGISTERED,
+    },
     {
       _id: 0,
       loginType: 0,
@@ -396,17 +385,11 @@ exports.getTradingWorkshopUsers = catchAsync(async (req, res, next) => {
       eHackTeamRole: 0,
       impetusTeamRole: 0,
       innoventureTeamRole: 0,
+      registeredEvents: 0,
       date: 0,
       __v: 0,
     }
   );
-
-  const tradingWorkshopRegisteredUsers = [];
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].registeredEvents && users[i].registeredEvents[4] === 1) {
-      tradingWorkshopRegisteredUsers.push(users[i]);
-    }
-  }
 
   res.status(200).json({
     message: "Data Fetched Successfully",
@@ -436,35 +419,10 @@ exports.getEhackDetails = catchAsync(async (req, res, next) => {
       _id: 0,
     });
 
-  // const ehackTeamsWith1Member = [];
-  // const ehackTeamsWith2Members = [];
-  // const ehackTeamsWith3Members = [];
-  // const ehackTeamsWith4Members = [];
-
-  // for (let i = 0; i < eHackTeams.length; i++) {
-  //   if (eHackTeams[i].members.length === 1) {
-  //     ehackTeamsWith1Member.push(eHackTeams[i]);
-  //   } else if (eHackTeams[i].members.length === 2) {
-  //     ehackTeamsWith2Members.push(eHackTeams[i]);
-  //   } else if (eHackTeams[i].members.length === 3) {
-  //     ehackTeamsWith3Members.push(eHackTeams[i]);
-  //   } else if (eHackTeams[i].members.length === 4) {
-  //     ehackTeamsWith4Members.push(eHackTeams[i]);
-  //   }
-  // }
-
   return res.status(200).json({
     message: "Data Fetched Successfully",
     No_of_EHack_Teams: eHackTeams.length,
     eHackTeams,
-    // No_of_EHack_Teams_With_1_Member: ehackTeamsWith1Member.length,
-    // No_of_EHack_Teams_With_2_Members: ehackTeamsWith2Members.length,
-    // No_of_EHack_Teams_With_3_Members: ehackTeamsWith3Members.length,
-    // No_of_EHack_Teams_With_4_Members: ehackTeamsWith4Members.length,
-    // E_Hack_Teams_With_1_Member: ehackTeamsWith1Member,
-    // E_Hack_Teams_With_2_Members: ehackTeamsWith2Members,
-    // E_Hack_Teams_With_3_Members: ehackTeamsWith3Members,
-    // E_Hack_Teams_With_4_Members: ehackTeamsWith4Members,
   });
 });
 
@@ -488,35 +446,10 @@ exports.getImpetusDetails = catchAsync(async (req, res, next) => {
       _id: 0,
     });
 
-  // const impetusTeamsWith1Member = [];
-  // const impetusTeamsWith2Members = [];
-  // const impetusTeamsWith3Members = [];
-  // const impetusTeamsWith4Members = [];
-
-  // for (let i = 0; i < impetusTeams.length; i++) {
-  //   if (impetusTeams[i].members.length === 1) {
-  //     impetusTeamsWith1Member.push(impetusTeams[i]);
-  //   } else if (impetusTeams[i].members.length === 2) {
-  //     impetusTeamsWith2Members.push(impetusTeams[i]);
-  //   } else if (impetusTeams[i].members.length === 3) {
-  //     impetusTeamsWith3Members.push(impetusTeams[i]);
-  //   } else if (impetusTeams[i].members.length === 4) {
-  //     impetusTeamsWith4Members.push(impetusTeams[i]);
-  //   }
-  // }
-
   return res.status(200).json({
     message: "Data Fetched Successfully",
     No_of_Impetus_Teams: impetusTeams.length,
     impetusTeams,
-    // No_of_Impetus_Teams_With_1_Member: impetusTeamsWith1Member.length,
-    // No_of_Impetus_Teams_With_2_Members: impetusTeamsWith2Members.length,
-    // No_of_Impetus_Teams_With_3_Members: impetusTeamsWith3Members.length,
-    // No_of_Impetus_Teams_With_4_Members: impetusTeamsWith4Members.length,
-    // Impetus_Teams_With_1_Member: impetusTeamsWith1Member,
-    // Impetus_Teams_With_2_Members: impetusTeamsWith2Members,
-    // Impetus_Teams_With_3_Members: impetusTeamsWith3Members,
-    // Impetus_Teams_With_4_Members: impetusTeamsWith4Members,
   });
 });
 
@@ -540,41 +473,9 @@ exports.getInnoventureDetails = catchAsync(async (req, res, next) => {
       _id: 0,
     });
 
-  // const innoventureTeamsWith1Member = [];
-  // const innoventureTeamsWith2Members = [];
-  // const innoventureTeamsWith3Members = [];
-  // const innoventureTeamsWith4Members = [];
-
-  // for (let i = 0; i < innoventureTeams.length; i++) {
-  //   if (innoventureTeams[i].members.length === 1) {
-  //     innoventureTeamsWith1Member.push(innoventureTeams[i]);
-  //   } else if (innoventureTeams[i].members.length === 2) {
-  //     innoventureTeamsWith2Members.push(innoventureTeams[i]);
-  //   } else if (innoventureTeams[i].members.length === 3) {
-  //     innoventureTeamsWith3Members.push(innoventureTeams[i]);
-  //   } else if (innoventureTeams[i].members.length === 4) {
-  //     innoventureTeamsWith4Members.push(innoventureTeams[i]);
-  //   }
-  // }
-
   return res.status(200).json({
     message: "Data Fetched Successfully",
     No_of_Innoventure_Teams: innoventureTeams.length,
     innoventureTeams,
-    // No_of_Innoventure_Teams_With_1_Member: innoventureTeamsWith1Member.length,
-    // No_of_Innoventure_Teams_With_2_Members: innoventureTeamsWith2Members.length,
-    // No_of_Innoventure_Teams_With_3_Members: innoventureTeamsWith3Members.length,
-    // No_of_Innoventure_Teams_With_4_Members: innoventureTeamsWith4Members.length,
-    // Innoventure_Teams_With_1_Member: innoventureTeamsWith1Member,
-    // Innoventure_Teams_With_2_Members: innoventureTeamsWith2Members,
-    // Innoventure_Teams_With_3_Members: innoventureTeamsWith3Members,
-    // Innoventure_Teams_With_4_Members: innoventureTeamsWith4Members,
   });
 });
-
-// exports.myFunction = catchAsync(async (req, res, next) => {
-//   await User.updateMany({}, { $set: { hasFilledDetails: false } });
-//   return res.status(200).json({
-//     status: "success",
-//   });
-// });
